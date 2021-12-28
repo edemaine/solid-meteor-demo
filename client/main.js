c051612e53ba5110212ef11c46452fceb6a66979
Erik Demaine <edemaine@mit.edu>
2021-12-28 13:33:04 -0500

    Mongo subscription and fetch demo (#2)
    * Toward adding Mongo demo
    
    * Finish Mongo

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
-import {createEffect, createRef, createSignal, onCleanup} from 'solid-js';
+import {createEffect, createSignal, onCleanup} from 'solid-js';
 import {render} from 'solid-js/web';
-import {Session} from 'meteor/session'
+import {Meteor} from 'meteor/meteor';
+import {Session} from 'meteor/session';
+import {Tracker} from 'meteor/tracker';
+
+import {ToDo} from '/lib/todo';
 
 function Hello(props) {
   return <h2>Hello {props.name}!</h2>;
@@ -21,6 +25,40 @@ function Timer(props) {
   return <h2>TIMER: {count}</h2>;
 }
 
+function TodoList(props) {
+  // Subscription
+  const sub = Meteor.subscribe('todo');
+  onCleanup(() => sub.stop());
+  // Query
+  const [todos, setTodos] = createSignal([]);
+  Tracker.autorun(() => setTodos(ToDo.find({}, {sort: {created: -1}}).fetch()));
+  // Display
+  let ref;
+  function onAdd(e) {
+    e.preventDefault();
+    Meteor.call('todo.add', ref.value);
+    ref.value = '';
+  }
+  function onDelete(e) {
+    Meteor.call('todo.del', e.target.parentNode.parentNode.dataset.id);
+  }
+  return <div>
+    <h2>To-Do List</h2>
+    <form onSubmit={onAdd}>
+      <input ref={ref}/>
+      <input type="submit" onClick={onAdd} value="Add Item"/>
+    </form>
+    <table>
+      <For each={todos()}>{(todo) =>
+        <tr data-id={todo._id}>
+          <td>{todo.title}</td>
+          <td class="date">{todo.created.toLocaleString()}</td>
+          <td><button onClick={onDelete}>Delete</button></td>
+        </tr>
+      }</For>
+    </table>
+  </div>;
+}
 
 function App(props) {
   const [name, setName] = createSignal(Session.get('name') || 'Solid');
@@ -30,6 +68,7 @@ function App(props) {
     <NameInput name={name()} setName={setName}/>
     <Hello name={name()}/>
     <Timer/>
+    <TodoList/>
   </>;
 }
 
