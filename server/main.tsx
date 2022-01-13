bf4b794d898a0b5200b9710af42f41a44eb07c12
Erik Demaine <edemaine@mit.edu>
2022-01-13 17:30:11 -0500

    Attempt at streaming SSR

--- server/main.tsx
+++ server/main.tsx
@@ -1,5 +1,6 @@
 import {onPageLoad} from 'meteor/server-render';
-import {generateHydrationScript, renderToString} from 'solid-js/web';
+import {generateHydrationScript, renderToString, renderToStream} from 'solid-js/web';
+import {Transform} from 'stream';
 
 import {solid} from '/package.json';
 import {App} from '/ui/main.tsx';
@@ -9,6 +10,16 @@ import '/lib/todo.ts';
 if (solid.ssr) {
   onPageLoad(sink => {
     sink.appendToHead!(generateHydrationScript());
-    sink.appendToBody!(renderToString(() => <App/>));
+    // Simple string-based server rendering
+    //sink.appendToBody!(renderToString(() => <App/>));
+    // Streaming server rendering
+    const stream = new Transform({
+      // @ts-ignore Unused argument 'encoding'
+      transform(data: any, encoding: any, callback: any) {
+        callback(null, data);
+      }
+    });
+    renderToStream(() => <App/>).pipe(stream);
+    sink.appendToBody!(stream);
   });
 }
